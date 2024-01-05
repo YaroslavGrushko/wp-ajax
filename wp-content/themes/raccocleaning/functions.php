1fb092ff2d1c542ac5f90426b97b0818fd0144e0
YaroslavGrushko <grushko.kpi@gmail.com>
2024-01-05 17:59:17 +0200

    Wp-ajax-int: init

---
+++ wp-content/themes/raccocleaning/functions.php
@@ -0,0 +1,27 @@
+<?php
+function raccocleaning_get_theme_version(){
+	$theme = wp_get_theme();
+	$theme_version = $theme->get('Version');
+	return $theme_version;
+}
+
+/* Main scripts and styles */
+ function raccocleaning_scripts(){
+	$theme_version = raccocleaning_get_theme_version();
+	// Theme's stylesheets:
+	wp_enqueue_style( 'raccocleaning-style', get_template_directory_uri() . '/style.css', array(), $theme_version, 'all' );
+	wp_enqueue_style( 'raccocleaning-critical-style', get_template_directory_uri() . '/assets/css/critical.css', array(), $theme_version, 'all' );
+	wp_enqueue_style( 'raccocleaning-app-style', get_template_directory_uri() . '/assets/css/app.css', array(), $theme_version, 'all' );
+	wp_enqueue_style( 'raccocleaning-fancybox-style', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), $theme_version, 'all' );
+
+	// Theme's javascript files:
+	wp_enqueue_script( 'lottie-js', get_template_directory_uri() . '/assets/js/lottie.min.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'inputmask-js', get_template_directory_uri() . '/assets/js/jquery.inputmask.min.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'jquery-validate-js', get_template_directory_uri() . '/assets/js/jquery.validate.min.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'additional-methods-js', get_template_directory_uri() . '/assets/js/additional-methods.min.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'jquery-fancybox-js', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'swiper-js', get_template_directory_uri() . '/assets/js/swiper.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_script( 'app-js', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), $theme_version, true );
+
+}
+add_action( 'wp_enqueue_scripts', 'raccocleaning_scripts' );
